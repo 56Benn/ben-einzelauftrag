77f6203e99ad26a0737a6534620962fb140aea63
TAAJEBE1 <ben.jenzer@swisscom.com>
2026-01-12 00:05:34 +0100

    updated all

--- backend/src/main/java/com/pruefungstipp/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.pruefungstipp.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
